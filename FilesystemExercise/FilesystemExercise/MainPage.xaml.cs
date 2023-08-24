d14f3bd19645624a34aaa8973b03c3b25b4fbd56
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-24 15:51:20 +0300

    feat: Clear only the buttons in the layout.

--- FilesystemExercise/FilesystemExercise/MainPage.xaml.cs
+++ FilesystemExercise/FilesystemExercise/MainPage.xaml.cs
@@ -4,6 +4,7 @@ namespace FilesystemExercise
 {
     public partial class MainPage : ContentPage
     {
+        List<Button> driveButtons = new List<Button>();
 
         public MainPage()
         {
@@ -15,14 +16,22 @@ namespace FilesystemExercise
         {
             var driveInfo = DriveInfo.GetDrives();
 
-            leftVerticalLayout.Children.Clear();
+            foreach(var button in driveButtons)
+            {
+                leftVerticalLayout.Children.Remove(button);
+            }
+
+            driveButtons.Clear();
+
             foreach (var drive in driveInfo)
             {
                 Button button = new()
                 {
-
                     Text = "(" + drive.Name + ") " + drive.VolumeLabel
                 };
+
+                driveButtons.Add(button);
+
                 leftVerticalLayout.Children.Add(button);
             }
         }
