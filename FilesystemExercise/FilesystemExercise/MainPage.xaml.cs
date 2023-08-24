31279100a90834feee64affb21e9b18afdd04dcb
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-24 16:07:05 +0300

    feat: Forward the drive info on click.

--- FilesystemExercise/FilesystemExercise/MainPage.xaml.cs
+++ FilesystemExercise/FilesystemExercise/MainPage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace FilesystemExercise
 {
     public partial class MainPage : ContentPage
     {
-        List<Button> driveButtons = new List<Button>();
+        List<Button> driveButtons = new();
 
         public MainPage()
         {
@@ -16,7 +17,7 @@ namespace FilesystemExercise
         {
             var driveInfo = DriveInfo.GetDrives();
 
-            foreach(var button in driveButtons)
+            foreach (var button in driveButtons)
             {
                 leftVerticalLayout.Children.Remove(button);
             }
@@ -27,13 +28,33 @@ namespace FilesystemExercise
             {
                 Button button = new()
                 {
-                    Text = "(" + drive.Name + ") " + drive.VolumeLabel
+                    Text = "(" + drive.Name + ") " + drive.VolumeLabel,
+                    BindingContext = drive,
+
                 };
+
+                button.Clicked += (sender, e) =>
+                {
+                    if (sender is Button button)
+                    {
+                        OnDriveButtonClicked(button.BindingContext as DriveInfo);
+                    }
+                    else
+                    {
+                        Debug.Assert(false, "The function is called by an unknown type");
+                    }
+                };
+
 
                 driveButtons.Add(button);
 
                 leftVerticalLayout.Children.Add(button);
             }
+        }
+
+        public void OnDriveButtonClicked(DriveInfo driveInfo)
+        {
+            Console.WriteLine(driveInfo.Name);
         }
     }
 }
