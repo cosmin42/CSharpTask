d877748b4f75c9d47eb25de05809f333d778d80a
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-24 16:39:28 +0300

    feat: Add TaskConsumerListener.

---
+++ FilesystemExercise/FilesystemExercise/TaskConsumerListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesystemExercise
+{
+    interface TaskConsumerListener
+    {
+        void cancelled();
+        void stopped();
+        void finished();
+    }
+}
